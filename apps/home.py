7367c98c22d87b259a2305401249ecb88ec368a6
taseersuleman <75067547+taseersuleman@users.noreply.github.com>
2021-04-09 09:33:53 -0700

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 def app():
     st.subheader("""
-    Cancer is driven by distinctive sorts of changes and basic variations in genes. Recognizing cancer driver genes is basic for accurate oncological analysis. Numerous progression calculations to distinguish drivers presently exist, but efficient tools to combine and optimize them on huge datasets are few. Most strategies for prioritizing transformations depend basically on frequency-based criteria. Strategies are required to dependably prioritize organically dynamic driver changes over inert passengers in high-throughput sequencing cancer information sets. """)
+    CCCancer is driven by distinctive sorts of changes and basic variations in genes. Recognizing cancer driver genes is basic for accurate oncological analysis. Numerous progression calculations to distinguish drivers presently exist, but efficient tools to combine and optimize them on huge datasets are few. Most strategies for prioritizing transformations depend basically on frequency-based criteria. Strategies are required to dependably prioritize organically dynamic driver changes over inert passengers in high-throughput sequencing cancer information sets. """)
     image = Image.open('cancer_01.jpg')
     st.image(image)
     st.subheader("""
